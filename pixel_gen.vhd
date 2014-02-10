a2234cd8d7b5191af97093f2f0451105f739e100
unknown <C15Ryan.Good@c15ragood.usafa.ds.af.edu>
2014-02-10 15:50:31 -0700

    putting the A together. Still struggling to work with how the monitor is oriented

--- pixel_gen.vhd
+++ pixel_gen.vhd
@@ -6,7 +6,7 @@
 -- Design Name: 
 -- Module Name:    pixel_gen - Behavioral 
 -- Project Name: 
--- Target Devices: 
+------	-- Target Devices: 
 -- Tool versions: 
 -- Description: 
 --
@@ -29,8 +29,12 @@ use IEEE.NUMERIC_STD.ALL;
 library UNISIM;
 use UNISIM.VComponents.all;
 
+library work;
+
+use work.af_constants.all;
+
 entity pixel_gen is
-port ( row : in unsigned(10 downto 0);
+port ( 	  row : in unsigned(10 downto 0);
            column : in unsigned(10 downto 0);
            blank : in std_logic;
            r : out std_logic_vector(7 downto 0);
@@ -41,9 +45,41 @@ end pixel_gen;
 architecture pixel_arch of pixel_gen is
 begin
 
-process (row, column, blank)
-begin
+	process (row, column, blank)
+	begin	
+		--initial status
+		r <= (others => '0');
+		g <= (others => '0');
+		b <= (others => '0');
+		
+		--Left side of A
+		if (row >= AF_Start_Row and row <= AF_Start_Row + Vertical_Bar_Size) then
+			if (column >= A_Start_Column and column <= A_Start_Column + Standard_Width) then
+				r <= (others => '0');
+				g <= (others => '0');
+				b <= (others => '1');
+			end if;
+		end if;
+		
+--		Top of A
+		if (row <= AF_Start_Row and row >= AF_Start_Row - Standard_Width) then
+			if (column >= A_Start_Column and column <= Horizontal_Bar_Size + A_Start_Column) then
+				r <= (others => '0');
+				g <= (others => '0');
+				b <= (others => '1');
+			end if;
+		end if;
+		
+--		Middle of A
+		if (row <= AF_Start_Row + Vertical_Bar_Size/2 and
+			row >= AF_Start_Row - Vertical_Bar_Size/2 - Standard_Width) then
+			if (column >= A_Start_Column and column <= Horizontal_Bar_Size + A_Start_Column) then
+				r <= (others => '0');
+				g <= (others => '0');
+				b <= (others => '1');
+			end if;
+		end if;
 
-end process;
+	end process;
 
 end pixel_arch;
