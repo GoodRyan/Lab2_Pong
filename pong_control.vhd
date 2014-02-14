ae788bfbfa87bbd756626c22a42862d9f688a54c
unknown <C15Ryan.Good@c15ragood.usafa.ds.af.edu>
2014-02-14 01:41:10 -0700

    put in the neccessary logic to have the ball bounce from left to right, next I will attempt to have the ball also bounce up and down

--- pong_control.vhd
+++ pong_control.vhd
@@ -48,6 +48,7 @@ end pong_control;
 
 architecture pong_control_arch of pong_control is
 
+
 component button_pressed
 	port(
 		clk         : in std_logic;
@@ -57,8 +58,13 @@ component button_pressed
 		);
 end component;
 
+type state_type is (movement, right_wall, left_wall);
+signal state_reg, state_next : state_type;
 signal up_signal, down_signal : std_logic;
 signal paddle_y_next, paddle_y_reg : unsigned(10 downto 0);
+signal counter_reg, counter_next : unsigned(10 downto 0);
+signal ball_x_reg, ball_x_next, ball_y_reg, ball_y_next : unsigned(10 downto 0);
+signal y_dir, x_dir, y_dir_reg, x_dir_reg: std_logic;
 begin
 
 inst_up_pressed: button_pressed
@@ -76,7 +82,96 @@ inst_down_pressed: button_pressed
 		button_in => down,
 		button_out => down_signal
 	);
+	
+--ball counter
+	counter_next <= counter_reg + 1 when counter_reg < to_unsigned(ball_speed, 11) and v_completed = '1' else
+					    (others => '0') when counter_reg >= to_unsigned(ball_speed, 11) else
+					    counter_reg;
+	
+	process(clk, reset)
+	begin
+		if reset = '1' then
+			counter_reg <= (others => '0');
+		elsif rising_edge(clk) then
+			counter_reg <= counter_next;
+		end if;
+	end process;
 
+--State Register
+process(clk, reset)
+begin
+	if reset = '1' then
+		state_reg <= movement;
+	elsif rising_edge(clk) then
+		state_reg <= state_next;
+	end if;
+end process;
+
+--output buffer
+process(clk, reset)
+	begin
+		if (reset = '1') then
+			ball_x_reg <= to_unsigned(ball_x_init, 11);
+			ball_y_reg <= to_unsigned(ball_y_init, 11);
+			x_dir_reg <= '0';
+		elsif (rising_edge(clk)) then
+			ball_x_reg <= ball_x_next;
+			ball_y_reg <= ball_y_next;
+			x_dir_reg <= x_dir;
+			y_dir_reg <= y_dir;
+		end if;
+	end process;
+	
+--next-state logic
+	process(state_next, state_reg, ball_x_reg)
+	begin
+		state_next <= state_reg;
+	if (counter_reg >= ball_speed) then
+	
+		case state_reg is
+			when movement =>
+				if (ball_x_reg >= horizontal_size-1) then
+					state_next <= right_wall;
+				elsif (ball_x_reg <= 1) then
+					state_next <= left_wall;
+				end if;
+			when right_wall =>	
+				state_next <= movement;
+			when left_wall =>
+				state_next <= movement;			
+		end case;
+		
+	end if;
+		
+	end process;
+	
+--output logic
+	process(state_next, counter_reg)
+	begin
+		ball_x_next <= ball_x_reg;
+		ball_y_next <= ball_y_reg;
+		x_dir <= x_dir_reg;
+		y_dir <= y_dir_reg;
+	if (counter_reg >= ball_speed) then
+		case state_next is
+			when movement =>
+				if (x_dir_reg = '0') then
+					ball_x_next <= ball_x_reg + 1;
+				elsif (x_dir_reg = '1') then
+					ball_x_next <= ball_x_reg - to_unsigned(1, 11);
+				end if;
+			when right_wall =>
+				x_dir <= '1';
+			when left_wall =>
+				x_dir <= '0';			
+		end case;
+	end if;
+	
+	end process;
+
+--output
+ball_x <= ball_x_reg;
+ball_y <= ball_y_reg;
 
 --paddle flip flop
 process(clk, reset)
@@ -95,9 +190,9 @@ begin
 	paddle_y_next <= paddle_y_reg;
 	
 	if (up_signal = '1' and  down_signal = '0' and paddle_y_next > 1) then
-		paddle_y_next <= paddle_y_reg - to_unsigned(1, 11);	
+		paddle_y_next <= paddle_y_reg - to_unsigned(5, 11);	
 	elsif (up_signal = '0' and down_signal = '1' and paddle_y_next < 479) then
-		paddle_y_next <= paddle_y_reg + 1;
+		paddle_y_next <= paddle_y_reg + 5;
 	end if;
 
 end process;
